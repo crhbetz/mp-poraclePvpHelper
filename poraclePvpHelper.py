a14e3581f290d1980a170ae908bce0fbb94bd74b
Christian Betz <christian@betz.cc>
2020-10-24 19:20:19 +0200

    add custom enum info from additional-enum-info.ini

--- poraclePvpHelper.py
+++ poraclePvpHelper.py
@@ -21,7 +21,17 @@ def parseEnumProto(url, name):
     for line in r.iter_lines(decode_unicode=True):
         if not line.startswith("syntax") and not line.startswith("package") and "=" in line:
             enumDict[line.split("=")[0].strip()] = line.split("=")[1].replace(";", "").strip()
+    enumDict = addEnumInfo(name, enumDict)
     return Enum(name, enumDict)
+
+
+def addEnumInfo(name, enumDict):
+    additionalInfo = configparser.ConfigParser()
+    additionalInfo.read(os.path.dirname(os.path.abspath(__file__)) + "/additional-enum-info.ini")
+    if name in additionalInfo:
+        for elem in additionalInfo[name]:
+            enumDict[elem.upper()] = additionalInfo[name][elem]
+    return enumDict
 
 
 PokemonId = parseEnumProto("https://raw.githubusercontent.com/Furtif/POGOProtos/master/src/"
